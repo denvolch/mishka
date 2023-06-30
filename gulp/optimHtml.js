e3eda490765219235c49cfd90106226e8ae8dee0
denvolch <wolkov.web@yandex.ru>
2023-06-30 23:33:11 +0300

    remove htmlmin

--- gulp/optimHtml.js
+++ gulp/optimHtml.js
@@ -1,14 +1,9 @@
 const { src, dest }  = require('gulp')
-const htmlmin = require('gulp-htmlmin')
-const { browserSync } = require('./server')
-
 
 
 const optimHtml = () => {
     return src('src/*.html')
-        // .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(dest('dist/'))
-        .pipe(browserSync.stream())
 }
 
 exports.optimHtml = optimHtml
